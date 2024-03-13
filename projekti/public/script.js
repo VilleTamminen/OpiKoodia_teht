e293c321312455f136a6558feee26beae729f13d
VilleTamminen <48275871+VilleTamminen@users.noreply.github.com>
2024-03-13 12:49:57 +0200

    python ja projekti alku

---
+++ projekti/public/script.js
@@ -0,0 +1,10 @@
+/*
+https://github.com/SelbyJGomes/R-Place/tree/master 
+https://codepen.io/ste-vg/pen/dvLvzy 
+https://www.youtube.com/watch?v=XSw5fFo0_pA 
+https://github.com/dynastic/place
+*/
+class Pixel 
+{
+	
+}
