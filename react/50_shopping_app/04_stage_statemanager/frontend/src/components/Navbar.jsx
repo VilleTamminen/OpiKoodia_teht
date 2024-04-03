1f21067aefdf364e1ff37528d99d841506afe9f9
VilleTamminen <tamminenville@hotmail.com>
2024-04-03 13:23:05 +0300

    passportjs

---
+++ react/50_shopping_app/04_stage_statemanager/frontend/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import {Link} from 'react-router-dom';
+import useAction from '../hooks/useAction';
+import useAppState from '../hooks/useAppState';
+
+//ei käytetä enään props
+const Navbar = () => {
+
+    const {logout} = useAction();
+	const {user,isLogged} = useAppState()
+	
+	if(isLogged) {
+		return(
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<p className="navbar-brand" style={{marginLeft:10}}>Shopping App</p>
+				<ul className="navbar-nav">
+					<li className="nav-item" style={{marginLeft:10}}>
+						<Link to="/" className="nav-link">Shopping List</Link>
+					</li>
+					<li className="nav-item" style={{marginLeft:10}}>
+						<Link to="/form" className="nav-link">Add new item</Link>
+					</li>
+					<li className="nav-item" style={{marginLeft:10}}>
+						<p className="nav-link" style={{color:"blue"}}>Logged in as {user}</p>
+					</li>
+					<li className="nav-item" style={{marginLeft:10}}>
+						<Link to="/" className="nav-link" onClick={logout}>Logout</Link>
+					</li>						
+				</ul>
+			</nav>
+		)
+	} else {
+		return(
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<p className="navbar-brand" style={{marginLeft:10}}>Shopping App</p>
+			</nav>
+		)
+		
+	}
+}
+
+export default Navbar;
---
+++ react/50_shopping_app/04_stage_statemanager/frontend/src/context/ActionContext.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ActionContext = React.createContext();
+
+ActionContext.displayName = "ActionContext";
+
+export default ActionContext;
+
